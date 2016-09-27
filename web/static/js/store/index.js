2495f20c5d6c504f7150b7b6d8562bc9486953e2
AlexKVal <alexkval@gmail.com>
2016-09-27 11:17:18 +0300

    Add redux store

---
+++ web/static/js/store/index.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware } from 'redux'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { syncHistory } from 'react-router-redux'
+import reducer from '../reducers'
+
+const loggerMiddleware = createLogger({
+  level: 'info',
+  collapsed: true,
+})
+
+export default function configureStore(browserHistory) {
+  const reduxRouterMiddleware = syncHistory(browserHistory)
+  const createStoreWithMiddleware = applyMiddleware(
+    reduxRouterMiddleware,
+    thunkMiddleware,
+    loggerMiddleware,
+  )(createStore)
+
+  return createStoreWithMiddleware(reducer)
+
+  // return createStore(
+  //   reducer,
+  //   preloadedState,
+  //   applyMiddleware(
+  //     reduxRouterMiddleware,
+  //     thunkMiddleware,
+  //     loggerMiddleware,
+  //   )
+  // )
+}
