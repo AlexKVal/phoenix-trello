21f85d097c73e6e1b38937df90cf83d695835085
AlexKVal <alexkval@gmail.com>
2016-09-30 22:09:51 +0300

    Adjust sessionReducer()

--- web/static/js/reducers/session.js
+++ web/static/js/reducers/session.js
@@ -1,9 +1,15 @@
+import Constants from '../constants'
+
 const initialState = {
-  currentUser: null,
-  socket: null,
-  error: null
+  currentUser: null
 }
 
 export default function sessionReducer (state = initialState, action = {}) {
-  return state
+  switch (action.type) {
+    case Constants.CURRENT_USER:
+      return { ...state, currentUser: action.currentUser }
+
+    default:
+      return state
+  }
 }
