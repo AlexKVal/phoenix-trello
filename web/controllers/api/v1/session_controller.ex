d124d8d04366eece363da530218c278d7cc5f1d1
AlexKVal <alexkval@gmail.com>
2016-09-30 22:46:01 +0300

    Add current user controller

--- web/controllers/api/v1/session_controller.ex
+++ web/controllers/api/v1/session_controller.ex
@@ -20,4 +20,10 @@ defmodule PhoenixTrello.SessionController do
         |> render("error.json")
     end
   end
+
+  def unauthenticated(conn, _params) do
+    conn
+    |> put_status(:forbidden)
+    |> render(PhoenixTrello.SessionView, "forbidden.json", error: "Not Authenticated")
+  end
 end
